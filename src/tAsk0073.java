3437f2495ec543fef7732ce8fbb856b26f19e71e
MOHAMED EL WASSIF <WASSIFMOHAMED@GMAIL.COM>
2023-01-21 21:42:11 -0500

    st

---
+++ src/tAsk0073.java
@@ -0,0 +1,9 @@
+public class tAsk0073 {
+    public static void main(String[] args) {
+        String[] arr ={"This" , "is" , "arrays" , "of" , "strings" };
+        for (int i = 0; i < 5; i++) {
+            System.out.print(arr[i]);
+
+        }
+    }
+}
